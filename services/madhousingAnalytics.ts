825f34587a84b3152580f41f38171209faf5529c
Aayush <abbharadwaj@wisc.edu>
2024-05-08 01:42:00 -0500

    add client key

--- services/madhousingAnalytics.ts
+++ services/madhousingAnalytics.ts
@@ -6,7 +6,12 @@ const propertyId = '312798667';
 
 // Using a default constructor instructs the client to use the credentials
 // specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
-const analyticsDataClient = new BetaAnalyticsDataClient();
+const analyticsDataClient = new BetaAnalyticsDataClient({
+  credentials: {
+    client_email: process.env.CLIENT_EMAIL,
+    private_key: process.env.PRIVATE_KEY,
+  },
+});
 
 export async function getTotalUsers() {
   const [response] = await analyticsDataClient.runReport({

8698f1c99a7d1bc947ef069419406949b2afd9c9
Aayush <abbharadwaj@wisc.edu>
2024-05-07 20:03:17 -0500

    Skeleton Code

---
+++ services/madhousingAnalytics.ts
@@ -0,0 +1,55 @@
+"use server";
+
+import {BetaAnalyticsDataClient} from '@google-analytics/data';
+
+const propertyId = '312798667';
+
+// Using a default constructor instructs the client to use the credentials
+// specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
+const analyticsDataClient = new BetaAnalyticsDataClient();
+
+export async function getTotalUsers() {
+  const [response] = await analyticsDataClient.runReport({
+    property: `properties/${propertyId}`,
+    dateRanges: [
+      {
+        startDate: '2022-04-01',
+        endDate: 'today',
+      },
+    ],
+    metrics: [
+      {
+        name: 'totalUsers',
+      },
+    ],
+  });
+
+  if (!response.rows || !response.rows[0].metricValues) {
+    throw new Error('No data found');
+  }
+
+  return response.rows[0].metricValues[0].value;
+}
+
+export async function getTotalPageViews() {
+  const [response] = await analyticsDataClient.runReport({
+    property: `properties/${propertyId}`,
+    dateRanges: [
+      {
+        startDate: '2022-04-01',
+        endDate: 'today',
+      },
+    ],
+    metrics: [
+      {
+        name: 'screenPageViews',
+      },
+    ],
+  });
+
+  if (!response.rows || !response.rows[0].metricValues) {
+    throw new Error('No data found');
+  }
+
+  return response.rows[0].metricValues[0].value;
+}
